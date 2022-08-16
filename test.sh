760ee8164c67e2783e5a4936b79b1da014a3ade4
cooksey <suitcase24@gmail.com>
2022-08-16 13:02:02 -0400

    Test

---
+++ test.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo "sketch_paths=\"$(
+    if [ 8 -ge 8 ];
+    then
+        echo 'yo'
+    elif [8 -ge 4 ]
+    then
+        echo 'hi'
+    else
+        echo 'end'
+    fi
+)\""
